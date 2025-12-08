92278d46916ea26200c456f4bba5389eb651527e
Noah Cohn <noahc42@gmail.com>
2025-12-08 02:31:54 -0800

    fix

--- src/components/RiverTrack.jsx
+++ src/components/RiverTrack.jsx
@@ -7,11 +7,11 @@ import { useTexture } from '@react-three/drei';
 export default function RiverTrack() {
     // 1. Reuse your textures
     const [colorMap, normalMap, roughnessMap, aoMap, displacementMap] = useTexture([
-        '/Rock031_1K-JPG_Color.jpg',
-        '/Rock031_1K-JPG_NormalGL.jpg',
-        '/Rock031_1K-JPG_Roughness.jpg',
-        '/Rock031_1K-JPG_AmbientOcclusion.jpg',
-        '/Rock031_1K-JPG_Displacement.jpg',
+        './Rock031_1K-JPG_Color.jpg',
+        './Rock031_1K-JPG_NormalGL.jpg',
+        './Rock031_1K-JPG_Roughness.jpg',
+        './Rock031_1K-JPG_AmbientOcclusion.jpg',
+        './Rock031_1K-JPG_Displacement.jpg',
     ]);
 
     useEffect(() => {
